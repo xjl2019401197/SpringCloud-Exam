ec294146d0f0c8dab1d6d5366d3d8cced0a94688
xjl2019401197 <2550908862@qq.com>
2022-06-22 11:09:26 +0800

    第三次提交，解决了登录后没有权限的问题

---
+++ springcloud-consumer-order/src/main/java/com/xu/config/SecurityConfigTest.java
@@ -0,0 +1,47 @@
+package com.xu.config;
+
+import com.xu.redis.JedisUtil;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import redis.clients.jedis.Jedis;
+
+@Configuration
+//开启注解securedEnabled、prePostEnabled
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
+public class SecurityConfigTest extends WebSecurityConfigurerAdapter {
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        //自定义403页面
+        http.exceptionHandling().accessDeniedPage("/unauth.html");
+        //自定义退出页面
+        Jedis jedis = JedisUtil.getJedis();
+//        if(path.contains("admin") && jedis.get("admin") != null|| path.contains("teacher") && jedis.get("teacher") != null ||path.contains("student") && jedis.get("student") != null)
+//        return chain.filter(exchange);
+        if (jedis.get("admin") != null || jedis.get("teacher") != null || jedis.get("student") != null) {
+            http.authorizeRequests().antMatchers("/**").permitAll();
+        }
+        http.logout().logoutUrl("/logout").logoutSuccessUrl("/test/hello").permitAll();
+        http.formLogin()        //自定义登录页面
+                .loginPage("/login")                //登录页面
+                .loginProcessingUrl("/tologin")      //登录访问路径
+                .permitAll()       //登录成功后，跳转的路径
+                .and().authorizeRequests()
+                .antMatchers("/login", "/register", "/tologin", "/toregister").permitAll()      //设置不需要认证路径
+                .antMatchers("/static/**").permitAll()      //设置不需要认证路径
+                .anyRequest().authenticated()           //全部需要认证
+                .and().csrf().disable();                 //csrf防护关闭
+        http.headers().frameOptions().disable();
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+
+        web.ignoring().antMatchers("/css/**", "/js/**", "/index.html", "/img/**", "/fonts/**", "/favicon.ico", "/verifyCode");
+    }
+
+}
